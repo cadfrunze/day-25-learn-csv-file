f9d1870c85a10a424ea4f57d0fc0d76770d43bb6
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2023-01-13 22:31:11 +0200

    update

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import csv
+# import csv
+#
+# with open("weater_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatura = []
+#     for rand in data:
+#         new_element = rand
+#         if new_element[1] == "temp":
+#             pass
+#         else:
+#             element = new_element[1]
+#             element = int(element)
+#             temperatura.append(element)
+#     print(temperatura)
 
-with open("weater_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatura = []
-    for rand in data:
-        new_element = rand
-        if new_element[1] == "temp":
-            pass
-        else:
-            element = new_element[1]
-            element = int(element)
-            temperatura.append(element)
-    print(temperatura)
+import pandas as pd
+
+data = pd.read_csv("weater_data.csv")
+print(data)
