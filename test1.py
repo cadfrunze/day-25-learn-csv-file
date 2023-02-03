003af5992261d57663be9c75fbed1ede293e639c
cadfrunze <marius1890@yahoo.com>
2023-02-03 10:07:44 +0200

    update

--- test1.py
+++ test1.py
@@ -1,4 +1,7 @@
-a = [1, 2, 3, "a", 7]
+# a = [1, 2, 3, "a", 7]
+#
+# b = a.index(3)
+# print(b)
 
-b = a.index(3)
-print(b)
+# with open("work_log.txt", 'a') as fisier:
+#     fisier.writelines('03.02\n')
